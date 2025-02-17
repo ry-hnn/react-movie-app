4fa733c823c5de119631e5ef53a8c289bf49df4b
Ryan Hennessy <rhennessy.tech@outlook.com>
2025-02-17 13:52:31 -0500

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="searchIcon.svg" alt="search" />
+      
+        <input 
+        type="text" 
+        placeholder='Search through movies'
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </div>
+    
+    </div>
+  )
+}
+
+export default Search
